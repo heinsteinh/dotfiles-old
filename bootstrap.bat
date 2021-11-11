d6d60c20c13ed2c9248bb64381fa4f929301c2ba
Wheezy <heinsteinh@gmail.com>
2021-11-11 21:23:37 +0100

    Update bootstrap.bat

--- bootstrap.bat
+++ bootstrap.bat
@@ -5,6 +5,6 @@ rem set LINKCMD_F=mklink /h
 set LINKCMD_F=mklink
 set LINKCMD_D=mklink /d
 
-mklink /d    C:\Users\heins\.vim  C:\Users\heins\dotfiles\vimConf\vim
-mklink    C:\Users\heins\.vimrc  C:\Users\heins\dotfiles\vimConf\vimrc
+mklink /d    C:\Users\heins\vimfiles  C:\Users\heins\dotfiles\vimConf\vim
+mklink    C:\Users\heins\_vimrc  C:\Users\heins\dotfiles\vimConf\vimrc
 
